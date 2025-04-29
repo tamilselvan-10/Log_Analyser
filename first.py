e46cb50f849024e93030b9290418a4f2022f8e94
tamilselvan-10 <23z606@psgitech.ac.in>
2025-04-29 23:30:22 +0530

    specify details

--- first.py
+++ first.py
@@ -2,3 +2,6 @@ import logging
 import numpy as np
 import matflotlib.yplot as mtp
 
+x= numpy.data(x)
+''' basic for data are stored in the git hub for quick access and manage the function tto maintain the labeled function'''
+import math
